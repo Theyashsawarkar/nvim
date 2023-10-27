c3095e16103f53d3d17f141bdcf0544b0ce7806b
YASH  SAWARKAR <104064673+Theyashsawarkar@users.noreply.github.com>
2023-10-27 16:10:26 +0530

    Update keymaps.lua
    hidden file toggle added

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local neotree_keybindings = {
+	{
+		mode = "n",
+		keys = "h",
+		command = function()
+			local bufnr = vim.fn.bufnr("%")
+			if vim.fn.getbufvar(bufnr, "&filetype") == "NvimTree" then
+				require("neo-tree.command").execute({ toggle_hidden = true })
+			else
+				vim.fn.feedkeys("h", "n")
+			end
+		end,
+		desc = "Toggle Hidden Files",
+	},
+}
+
+return neotree_keybindings
