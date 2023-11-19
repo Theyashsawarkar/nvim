df25ebaa034bd8ec7a05ea5bbcf1e8add041897a
Theyashsawarkar <yashsawarkar01@gmail.com>
2023-11-19 18:52:19 +0530

    made tokyonight default theme and added neorg plugin for taking notes

--- lua/plugins/onedark.lua
+++ lua/plugins/onedark.lua
@@ -1,10 +1,13 @@
+--NOTE:-
+--      uncomment the below lines in function to make onedark default theme
+
 return {
   {
     "navarasu/onedark.nvim",
-    config = function()
-      vim.cmd("colorscheme onedark")
-    end,
-    run = "make", -- Optional: Run a command after the plugin is installed
-    priority = 1000, -- Set a high priority for this plugin
+    -- config = function()
+    --   vim.cmd("colorscheme onedark")
+    -- end,
+    -- -- run = "make", -- Optional: Run a command after the plugin is installed
+    -- priority = 1000, -- Set a high priority for this plugin
   },
 }
